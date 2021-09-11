ed7e06bd25d458c462cbffce825c0a09729ced40
nix-dev-tech <85816723+nix-dev-tech@users.noreply.github.com>
2021-09-11 12:13:58 -0400

    Added Insertion sort cpp file

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include<vector>
+
+using namespace std;
+
+void swap(int& num1, int& num2){
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+
+void InsertionSort(vector<int>& nums, bool desc = false){
+    for(int i = 1; i < nums.size(); i++){
+        for(int j = i; j > 0; j--){
+            if(desc){
+              //if desc flag is true, sort in descending order.
+                if(nums[j] > nums[j-1]){ //check if previous element is less than current and swap if yes else break
+                    swap(nums[j], nums[j-1]);
+                }
+                else{
+                    break;
+                }
+            }
+            else{
+                if(nums[j] < nums[j-1]){ //check if previous element is greater than current and swap if yes else break
+                    swap(nums[j], nums[j-1]);
+                }
+                else{
+                    break;
+                }
+            }
+        }
+    }
+}
+
+int main()
+{
+    vector<int>nums{7,4,6,2,1,3};
+    //ascending order
+    InsertionSort(nums);
+    for(auto num : nums){
+        cout<<num<<" ";
+    }
+    
+    cout<<endl;
+    
+    //descending order
+    InsertionSort(nums, true);
+    for(auto num : nums){
+        cout<<num<<" ";
+    }
+
+    return 0;
+}
